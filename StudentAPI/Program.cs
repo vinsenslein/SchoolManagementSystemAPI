443ab602f676cc5d81bb6951e82273b22662c480
Vinsen <vinsensiussugitolein@gmail.com>
2025-08-03 15:06:43 +0700

    08/03/2025

--- StudentAPI/Program.cs
+++ StudentAPI/Program.cs
@@ -1,5 +1,3 @@
-// Program.cs (Serilog + Semua Konfigurasi)
-
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 using Serilog.Events;
@@ -8,35 +6,22 @@ using SchoolManagementSystemAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Serilog Config
-Log.Logger = new LoggerConfiguration()
-    .MinimumLevel.Information()
-    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-    .Enrich.FromLogContext()
-    // Log ke console
-    .WriteTo.Console()
-    // Log harian ke folder khusus
-    .WriteTo.File(
-        path: @"D:\\Logs\\StudentAPI\\log-.txt",
-        rollingInterval: RollingInterval.Day
-    )
-    // Log error khusus ke file terpisah
-    .WriteTo.File(
-        path: @"D:\\Logs\\StudentAPI\\error-.txt",
-        rollingInterval: RollingInterval.Day,
-        restrictedToMinimumLevel: LogEventLevel.Error
-    )
-    // Format JSON log
-    .WriteTo.File(
-        formatter: new JsonFormatter(),
-        path: @"D:\\Logs\\StudentAPI\\json-log-.json",
-        rollingInterval: RollingInterval.Day
-    )
-    .CreateLogger();
+// Konfigurasi Serilog lengkap
+builder.Host.UseSerilog((context, services, loggerConfig) =>
+{
+    loggerConfig
+        .MinimumLevel.Information()
+        .MinimumLevel.Override("Microsoft", LogEventLevel.Warning) // Semua log Microsoft, minimal Warning
+        .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information) // Tapi ini tetap ditampilkan
+        .MinimumLevel.Override("Microsoft.AspNetCore.Hosting.Diagnostics", LogEventLevel.Warning) // Khusus hosting
+        .Enrich.FromLogContext()
+        .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {SourceContext} - {Message:lj}{NewLine}{Exception}")
+        .WriteTo.File(@"D:\Logs\StudentAPI\log-.txt", rollingInterval: RollingInterval.Day)
+        .WriteTo.File(@"D:\Logs\StudentAPI\error-.txt", rollingInterval: RollingInterval.Day, restrictedToMinimumLevel: LogEventLevel.Error)
+        .WriteTo.File(new JsonFormatter(), @"D:\Logs\StudentAPI\json-log-.json", rollingInterval: RollingInterval.Day);
+});
 
-builder.Host.UseSerilog();
-
-// Tambahkan layanan lainnya
+// Tambahkan service
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -52,8 +37,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseSerilogRequestLogging(); //responded log misal ok 200
 app.UseHttpsRedirection();
 app.UseAuthorization();
+
 app.MapControllers();
+
+// Jalankan aplikasi
 app.Run();
